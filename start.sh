3ec293b698c8c7c15462e042a1d8db9d657c6047
5225225 <5225225@users.noreply.github.com>
2015-02-19 14:30:04 +0000

    Add stop.sh to kill the server, rename manager.sh
    When the main server is killed, all the blocks will exit quickly. It's easier
    to keep track of one PID than PIDs for all of the blocks and the server

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/.config/dtach/bar
 python ~/src/bar/server.py &
-sleep 1
+echo "$!" > pidfile
+sleep .1
 python ~/src/bar/modules/timeblock.py & 
 python ~/src/bar/modules/mpd.py &
 python ~/src/bar/modules/load.py &
