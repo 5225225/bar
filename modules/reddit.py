ecc4e51471693b1b444cc9a2887cca20670ca5d2
5225225 <5225225@users.noreply.github.com>
2015-02-20 12:43:28 +0000

    Add module, reddit.py Prints unread message count
    Username/password is read from the file name in argv[1]. It can be changed
    from what I'm using, but add the file to the .gitignore.

---
+++ modules/reddit.py
@@ -0,0 +1,26 @@
+import sys
+import praw
+import signal
+import linelib
+
+def handler(x,y):
+    pass
+
+signal.signal(signal.SIGUSR1, handler)
+signal.signal(signal.SIGALRM, handler)
+
+ID = "reddit"
+
+r = praw.Reddit(user_agent="unread message checker by /u/5225225")
+auth = open(sys.argv[1])
+username, password = auth.read().split(":")
+auth.close()
+r.login(username, password)
+
+while True:
+    x = r.get_unread()
+    x = list(x)
+    if len(x) > 0:
+        linelib.sendblock(ID, {"full_text": "[{}]".format(str(len(x))), "color": "#ff4500"})
+    linelib.sendPID(ID)
+    linelib.waitsig(5)
--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ sleep .1
 python ~/src/bar/modules/timeblock.py & 
 python ~/src/bar/modules/mpd.py &
 python ~/src/bar/modules/load.py &
+python ~/src/bar/modules/reddit.py modules/reddit-auth &
 wait
