6ab0fc6d9b60fc02b12122a85482069a01a3e88c
5225225 <5225225@users.noreply.github.com>
2015-03-03 19:52:17 +0000

    Add a template to base new modules on.

---
+++ modules/template.py
@@ -0,0 +1,15 @@
+import signal
+import linelib
+
+def handler(x,y):
+    pass
+
+signal.signal(signal.SIGUSR1, handler)
+signal.signal(signal.SIGALRM, handler)
+
+ID = "template"
+
+while True:
+    linelib.sendblock(ID, {"full_text": ""})
+    linelib.sendPID(ID)
+    linelib.waitsig(1)
