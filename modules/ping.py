5def6d91ffec0d08903c35cab2ed0352fd8a8812
5225225 <5225225@users.noreply.github.com>
2015-03-03 20:04:25 +0000

    Add ping module to display ping to set server
    By default, the server is 8.8.8.8 (Google's DNS). This can be easily changed.

---
+++ modules/ping.py
@@ -0,0 +1,21 @@
+import signal
+import linelib
+import subprocess
+
+def handler(x,y):
+    pass
+
+signal.signal(signal.SIGUSR1, handler)
+signal.signal(signal.SIGALRM, handler)
+
+ID = "ping"
+
+toping = "8.8.8.8"
+
+while True:
+    raw = subprocess.check_output(["ping", "-c1" ,toping]).decode("UTF8").split("\n")
+    line = raw[1]
+    time = line.split(" ")[6][5:]
+    linelib.sendblock(ID, {"full_text": "{}ms".format(time)})
+    linelib.sendPID(ID)
+    linelib.waitsig(5)
